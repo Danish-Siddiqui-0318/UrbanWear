f9180194c1fecf6e45444b95b0cc52105bf4d101
Danish Siddiqui <contact.dasiddique@gmail.com>
2026-03-09 01:48:27 +0500

    fix API url for production

--- server/server.js
+++ server/server.js
@@ -17,9 +17,14 @@ const AnnouncementRoute = require("./routes/announcement_routes");
 const HeroSlideRoute = require("./routes/heroSlide_routes");
 
 // Standard MiddleWare
-app.use(express.json({ limit: '10kb' })); 
-app.use(cors());
-
+app.use(express.json({ limit: '10kb' }));
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://urbanwearpk.store"
+    ],
+    credentials: true
+}));
 // Security MiddleWare
 app.use(helmet()); // Set security HTTP headers
 app.use(hpp()); // Prevent parameter pollution
