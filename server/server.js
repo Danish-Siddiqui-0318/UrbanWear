cdecbb3428cb0a341598a088f85ef6316d34a8a4
Danish Siddiqui <contact.dasiddique@gmail.com>
2026-03-09 02:23:47 +0500

    fix cors for production domain

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "https://urbanwearpk.store",
-        "https://urbanwearpk-store.vercel.app"
+        "https://www.urbanwearpk.store"
     ],
     credentials: true
 }));
